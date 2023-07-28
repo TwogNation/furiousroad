4b6476274118ea9dd29bc5b209841a9fd1356e4f
Matheus N <matheusnaschaves@gmail.com>
2023-07-28 18:31:20 -0300

    ws

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -10,10 +10,46 @@ function postText(value){
 
 const scriptsInEvents = {
 
+	async _gameplay_main_Event12_Act1(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.onMessage = (event) => {
+			if (event.data.startsWith('s:')){
+				const score_ = event.data.split(':')[1];
+				console.log(score_);
+				runtime.globalVars.score = score_;
+				runtime.callFunction("updateScore")
+			}
+		}
+	},
+
+	async ["_timer&Scenarios_Event2_Act3"](runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:Current_Speed/100")
+	},
+
+	async _firstscreen_Event18_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('startendless');
+			};
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
+	},
+
 	async _win_score_Event8_Act6(runtime, localVars)
 	{
-		postText(runtime.globalVars.postMsg)
-		
+		runtime.globalVars.webSocket.close();
 	}
 
 };
